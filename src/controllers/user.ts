5836ce8b803c1cdccf8587d94faa7085d0d03aa5
Dahn Registrado <dahnregistrado05@gmail.com>
2024-08-26 16:33:45 +0800

    Functionality: PUT user information

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { createUser } from "services/user";
 import { successActionFormatterResponse } from "utils/formatter";
+import cleanDeep from "clean-deep";
+import UserModel from "models/user";
 
 export const postUser = async (
   req: Request,
@@ -70,6 +72,46 @@ export const getUserProfile = (
     res.status(200).json({
       data: data,
     });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const putUserProfile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { user } = req;
+  const { user_id } = user;
+  const { first_name, last_name, email, street, city, country } =
+    req.body as Pick<
+      USER,
+      | "first_name"
+      | "last_name"
+      | "email"
+      | "street"
+      | "city"
+      | "country"
+      | "password"
+    >;
+
+  const query = cleanDeep({
+    first_name,
+    last_name,
+    email,
+    street,
+    city,
+    country,
+  });
+
+  try {
+    await UserModel.update(query, { where: { user_id } });
+    res
+      .status(200)
+      .json(
+        successActionFormatterResponse("Successfully updated a user profile")
+      );
   } catch (err) {
     next(err);
   }
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { postUser, getUserProfile } from "controllers/user";
+import { postUser, getUserProfile, putUserProfile } from "controllers/user";
 import { checkSchema } from "express-validator";
 import { USER_VALIDATIONS } from "constants/validations";
 import { validationErrorHandler } from "middlewares/errors";
 import { jwtMiddleware } from "middlewares/jwt";
+import { pick } from "lodash";
 
 const route = Router();
 
@@ -17,6 +18,23 @@ const userRoutes = (app: Router) => {
   );
 
   route.get("/profile", jwtMiddleware, getUserProfile);
+  route.put(
+    "/profile",
+    jwtMiddleware,
+    validationErrorHandler(
+      checkSchema(
+        pick(USER_VALIDATIONS, [
+          "first_name",
+          "last_name",
+          "email",
+          "street",
+          "city",
+          "country",
+        ])
+      )
+    ),
+    putUserProfile
+  );
 };
 
 export default userRoutes;
